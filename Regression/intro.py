76f0f322446b7d53c6207dedaad5d0f31ca8bbb9
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-11-24 17:08:38 +0530

    commit

---
+++ Regression/intro.py
@@ -0,0 +1,13 @@
+"""
+Regression
+============================
+
+in regression the model will predict the continuous (without rounding off) values
+
+
+simple linear regression algorithm
+========================================
+Should calculate the error of the predicted value with actual value
+
+
+"""
