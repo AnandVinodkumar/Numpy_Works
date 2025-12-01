dbcb45deb2aeef088583b5ba56b879737b58aa04
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-12-01 15:23:02 +0530

    sdhga

---
+++ HyperParameterTuning/intro.py
@@ -0,0 +1,14 @@
+"""
+Hyper parameter tuning
+-----------------------------------
+Used to improve accuracy model
+The settings which are given to model before training
+They decide how the model learns
+
+knn = KNeighborsClassifier(n_neighbours = 7)
+
+rf = RandomForestClassifier(n_estimators = 100)
+
+Trying different combinations of hyperparameters and selecting the best one
+
+"""
