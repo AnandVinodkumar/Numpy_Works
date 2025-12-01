3ac8128697c1e9306b50437bf899957d3d16dc25
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-12-01 17:08:03 +0530

    sahfsg

---
+++ encoding.py
@@ -0,0 +1,32 @@
+# Label Encoding
+
+# It considers each value as ordinal
+
+# low  medium  high  >>> 0   1   2
+
+"""
+Label Encoding
+priority
+---------------------
+low     0
+medium  1
+high    2
+
+One Hot Encoder >>> an encoding method used to convert categorical data into numerical format (0s  and 1s)
+---------------------------
+
+priority    priority_low    priority_medium    priority_high
+-------------------------------------------------------------------
+low             1                0                    0
+
+medium          0                1                    0
+
+high            0                0                    0
+--------------------------------------------------------------
+
+
+
+
+
+
+"""
