eda289683e9f9db50e6f648193e17dad26e2e13d
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-12-03 17:09:31 +0530

    asas

---
+++ Performance Evaluation Metrics/intro.py
@@ -0,0 +1,12 @@
+"""
+accuracy_score >>>  correct  /  total predictions
+
+precision  >>>  TP / TP + FP
+
+recall  >>>   TP / TP + FN
+
+f1-score >>> (2 * precision * recall) / (precision + recall)
+
+confusion matrix >>> TP FP TN FN
+
+"""
