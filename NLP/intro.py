8bc6c6f381a2db8ca1bc361ca3d05376d7a482ac
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-12-10 17:05:45 +0530

    jkjfhg

---
+++ NLP/intro.py
@@ -0,0 +1,44 @@
+# Natural Language Processing
+# -------------------------------------------
+"""
+A field of AI that enables computers to understand,
+interpret and generate human language
+
+Converts our language into machine knowing language
+"""
+
+# Preprocessing steps (Optional)
+#--------------------------------------------------
+"""
+1) Remove unwanted white spaces
+2) Convert all characters into ASCII
+3) Expand shortforms
+4) Remove unwanted characters #%&*_-
+"""
+
+# Must be implemented steps
+"""
+django is a API mastering tool
+
+stopwords (almost 180 stopwords)
+
+Eg: Universities are amazing places for studying and learning new things
+stopwords are 'are','and','is','the','those','that',..................180
+
+Convert all characters to lowercase
+
+lemmatization / stemming >>> Removes -ing (Tail part of the words) from words to find the root words
+
+Tokenization
+--------------------------------------
+n-gram method >> takes n words from a sentence as a token
+
+"vinay is a good boy" Splits using whitespaces
+# vinay
+# is
+# a
+# good
+# boy
+this is 1-gram method
+
+"""
